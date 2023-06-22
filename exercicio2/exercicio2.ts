56b382025ee4f3407573f3530c865c171c9783b6
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-06-22 13:34:38 -0300

    Delete exercicio2 directory

--- exercicio2/exercicio2.ts
+++
@@ -1,6 +0,0 @@
-function printThreeFavoriteColors():void {
-    const cor1 = prompt("Insira sua primeira cor favorita")
-    const cor2 = prompt("Insira sua segunda cor favorita")
-    const cor3 = prompt("Insira sua terceira cor favorita")
-    console.log([cor1, cor2, cor3])
- }
