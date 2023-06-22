6f3064265bfb0bc4dd26af2429df8f17bc494330
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-06-22 20:25:50 -0300

    Add files via upload

---
+++ introducao/exercicio4/script.js
@@ -0,0 +1,18 @@
+function comparaDoisNumeros(num1, num2) {
+    let maiorNumero;
+    let menorNumero;
+  
+    if (num1 > num2) {
+      maiorNumero = num1;
+      menorNumero = num2;
+    } else {
+      maiorNumero = num2;
+      menorNumero = num1;
+    }
+  
+    const diferenca = maiorNumero - menorNumero;
+  
+    return diferenca 
+  }
+
+  console.log(comparaDoisNumeros(10,20));
