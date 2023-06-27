4a162c1a4954b2b6b27daec0b970b369909c297e
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-06-26 21:56:19 -0300

    Update script.ts

--- introducao/exercicio8/src/script.ts
+++ introducao/exercicio8/src/script.ts
@@ -8,3 +8,20 @@ let newSentence:string = newArray.join('');
 
 console.log(sentence);
 console.log(newSentence);
+
+/*exercício do professor
+Criar uma função que recebe o primeiro nome e o sobrenome, o sobrenome pode ser nulo, mas se vier,
+exibir nome completo
+*/
+
+let nome:string = 'João';
+let sobrenome:null;
+
+function fullName(primeiro:string, segundo:string | null):string{
+    let primeiroNome:string = primeiro;
+    let segundoNome:string | null = segundo;
+    let frase: string = `Nome: ${primeiroNome} ${segundoNome}`
+    return frase;
+}
+
+console.log(fullName(nome,null));
