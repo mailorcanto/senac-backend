dfe1d850ea05fbb5661c8ac9fbc3a29137ee31c6
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-06-27 20:14:22 -0300

    Delete exercicios27.06/exercicio2 directory

--- exercicios27.06/exercicio2/build/script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let nome = 'João da Silva';
-let idade = 50;
-function receberParametro(parametro) {
-    return console.log(typeof parametro);
-}
-receberParametro(nome);
-//# sourceMappingURL=script.js.map
