abb91021839ef75f7ab83af2576a388322e6c044
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-06-27 21:49:12 -0300

    Add files via upload

---
+++ exercicios27.06/exercicio2/build/script.js
@@ -0,0 +1,8 @@
+"use strict";
+let nome = 'João da Silva';
+let idade = 50;
+function receberParametro(parametro) {
+    return console.log(typeof parametro);
+}
+receberParametro(nome);
+//# sourceMappingURL=script.js.map
