b2a212f92b35419fdc91f18eb0a27c32eff6ad21
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-06-28 17:53:31 -0300

    Add files via upload

---
+++ exercicios27.06/exercicio8/build/script.js
@@ -0,0 +1,31 @@
+"use strict";
+/*
+Escreva uma função que pergunta ao usuário a data de nascimento de uma pessoa (ex.: “24/04/1993”, e a data de emissão da sua carteira de
+//identidade (ex.: “07/11/2015”). A função deve retornar um booleano que indica se a carteira precisa ser renovada ou não. A carteira precisa
+//ser renovada segundo os seguintes critérios:
+
+- Para pessoas com menos de 20 anos, ou exatamente 20 anos, deve ser renovada de 5 em 5 anos (se for exatamente 5 anos, deve ser renovada).
+- Para pessoas entre 20 e 50 anos, ou exatamente 50, deve ser renovada de 10 em 10 anos (se for exatamente 10 anos, deve ser renovada).
+- Para pessoas acima dos 50 anos, deve ser renovada de 15 em 15 anos
+*/
+//function renovarID ():any{
+let dataNasc = prompt('Informe sua data de nascimento: (dd/mm/aaaa)');
+let dataEmis = prompt('Informe a data de emissão da sua carteira: (dd/mm/aaaa)');
+let nasc = dataNasc?.split('/');
+let emis = dataEmis?.split('/');
+let nascReordenada = [nasc[2], nasc[1], nasc[0]].join("/");
+let emisReordenada = [emis[2], emis[1], emis[0]].join("/");
+const today = new Date();
+const dateString = today.toLocaleDateString();
+console.log(new Date(dateString).getTime() - nascReordenada.toLocaleDateString() < 20);
+if (dateString - nascReordenada < 20) {
+    if (dateString - emisReordenada > 5) {
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    }
+}
+//}
+//console.log(renovarID());
+//# sourceMappingURL=script.js.map
