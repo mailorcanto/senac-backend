8f3a5a42777c02dce0f30b20dd7f663e02cd08d1
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-06-28 21:15:58 -0300

    Update script.ts

--- exercicios27.06/exercicio4/src/script.ts
+++ exercicios27.06/exercicio4/src/script.ts
@@ -28,7 +28,7 @@ type funcionarios = {
 	presencial: boolean
 } 
 
-let arrayFuncionarios: {nome: string, salario: number, setor:string, presencial:boolean }[] = [
+let arrayFuncionarios: funcionarios [] = [
 	{ nome: "Marcos", salario: 2500, setor: "marketing", presencial: true },
 	{ nome: "Maria" ,salario: 1500, setor: "vendas", presencial: false},
 	{ nome: "Salete" ,salario: 2200, setor: "financeiro", presencial: true},
