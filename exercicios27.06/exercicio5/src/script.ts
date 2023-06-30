ee9900785a07e2a076fba8f95e38f0bab8562eaa
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-06-30 13:07:17 -0300

    Update script.ts

--- exercicios27.06/exercicio5/src/script.ts
+++ exercicios27.06/exercicio5/src/script.ts
@@ -1,22 +1,21 @@
 //Considerando o array de usuários abaixo, crie uma função que receba este array como parâmetro e retorne uma lista com apenas os emails
 //dos usuários “admin”. 
 
-type usuarios = {
-	name: string,
-	email: string,
-	role: string
-}
+let users = [
+    {name: "Rogério", email: "roger@email.com", role: "user"},
+	{name: "Ademir", email: "ademir@email.com", role: "admin"},
+	{name: "Aline", email: "aline@email.com", role: "user"},
+	{name: "Jéssica", email: "jessica@email.com", role: "user"},  
+	{name: "Adilson", email: "adilson@email.com", role: "user"},  
+	{name: "Carina", email: "carina@email.com", role: "admin"}
+];
 
-const arrayUsuarios: usuarios [] = 
-[{name: "Rogério", email: "roger@email.com", role: "user"},
-{name: "Ademir", email: "ademir@email.com", role: "admin"},
-{name: "Aline", email: "aline@email.com", role: "user"},
-{name: "Jéssica", email: "jessica@email.com", role: "user"},  
-{name: "Adilson", email: "adilson@email.com", role: "user"},  
-{name: "Carina", email: "carina@email.com", role: "admin"}];
+const result = users.filter((user) => {
+    return user.role === 'admin';
+});
 
-function arrayFiltrado (array:usuarios[]):void{
-	console.log(array.filter(emails => emails.role == 'admin'));
-}
+const emails = result.map((listFilter) => {    
+    return listFilter.email;
+});
 
-arrayFiltrado(arrayUsuarios);
+console.log(emails);
