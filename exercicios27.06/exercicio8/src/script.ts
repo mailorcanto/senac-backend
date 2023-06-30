38a9b083ab7c856168708572d28d82cdb087d0a6
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-06-30 18:19:41 -0300

    Update script.ts

--- exercicios27.06/exercicio8/src/script.ts
+++ exercicios27.06/exercicio8/src/script.ts
@@ -6,26 +6,49 @@ Escreva uma função que pergunta ao usuário a data de nascimento de uma pessoa
 - Para pessoas com menos de 20 anos, ou exatamente 20 anos, deve ser renovada de 5 em 5 anos (se for exatamente 5 anos, deve ser renovada).
 - Para pessoas entre 20 e 50 anos, ou exatamente 50, deve ser renovada de 10 em 10 anos (se for exatamente 10 anos, deve ser renovada).
 - Para pessoas acima dos 50 anos, deve ser renovada de 15 em 15 anos
+Dica 1: Você precisará da data atual para fazer as operações, uma opção é utilizar new Date() para obter a data atual
+Dica 2: Para fazer as operações necessárias, você pode converter as datas para timestamp usando o método getTime() na data 
 */
 
-//function renovarID ():any{
-	let dataNasc = prompt('Informe sua data de nascimento: (dd/mm/aaaa)');
-	let dataEmis = prompt('Informe a data de emissão da sua carteira: (dd/mm/aaaa)');
-	let nasc: any = dataNasc?.split('/');
-	let emis: any = dataEmis?.split('/');
-	let nascReordenada:any = [nasc[2], nasc[1], nasc[0]].join("/");
-	let emisReordenada:any = [emis[2], emis[1], emis[0]].join("/");
-	const today: Date = new Date();
-	const dateString: any = today.toLocaleDateString();
-	console.log(new Date(dateString).getTime() - nascReordenada.toLocaleDateString() < 20);
-	if (dateString - nascReordenada < 20){
-		if (dateString - emisReordenada > 5){
-			console.log(true);
+function renewId ():any{
+	let data: Date = new Date ();
+
+	let dataNasc: string | null |undefined = prompt('Informe sua data de nascimento');
+	let nasc: any[] = dataNasc.split('/');
+	let nascReordenada = new Date (`${nasc[2]},${nasc[1]},${nasc[0]}`);
+	var idadeMilisegundos = Math.abs(data.getTime() - nascReordenada.getTime()); //converte as duas datas para milisegundos e retorna a diferença entre elas
+	var idadeDias = Math.ceil(idadeMilisegundos / (1000 * 3600 * 24));  //transformando em segundos, dividindo pelos segundos de uma hora (3600)
+	//e multiplicando por 24h para transformar milisegundos em dias
+
+	let dataRenov:string = `10/10/2010`;
+	let renov: any[] = dataRenov.split('/');
+	let renovReordenada = new Date (`${renov[2]},${renov[1]},${renov[0]}`);
+	var renovMilisegundos = Math.abs(data.getTime() - renovReordenada.getTime());
+	var renovDias = Math.ceil(renovMilisegundos / (1000 * 3600 * 24))
+
+	if (idadeDias / 365 <= 20){ if(renovDias / 365 >= 5){
+			return console.log('renovar?',true);
 		}
 		else {
-			console.log(false);
+			return console.log('renovar?',false);
 		}
 	}
-//}
+	if (idadeDias / 365 >= 21 && idadeDias / 365 <=50){
+		if(renovDias / 365 >= 10){
+			return console.log('renovar?',true);
+		}
+		else {
+			return console.log('renovar?',false);
+		}
+	}
+	if (idadeDias / 365 > 50){
+		if(renovDias / 365 >= 15){
+			return console.log('renovar?',true);
+		}
+		else {
+			return console.log('renovar?',false);
+		}
+	}
+}
 
-//console.log(renovarID());
+renewId();
