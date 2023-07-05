f5b4b8946a64c0635b1fb3f1058a1a9856c6414c
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-07-04 21:27:34 -0300

    Update script.ts

--- exercicios27.06/exercicio10/src/script.ts
+++ exercicios27.06/exercicio10/src/script.ts
@@ -101,3 +101,32 @@ function validarCpf (array:number[]):void{
     }
 }
 validarCpf(arrayConvertido);
+
+//solução do professor
+function convertToRoman(year: number):string{
+  const values= [{symbol: 'M', value: 1000},
+                {symbol: 'CM', value: 900},
+                {symbol: 'D', value: 500},
+                {symbol: 'CD', value: 400},
+                {symbol: 'C', value: 100},
+                {symbol: 'XC', value: 90},
+                {symbol: 'L', value: 50},
+                {symbol: 'XL', value: 40},
+                {symbol: 'X', value: 10},
+                {symbol: 'IX', value: 9},
+                {symbol: 'V', value: 5},
+                {symbol: 'IV', value: 4},
+                {symbol: 'I', value: 1}]
+
+  let result:string = '';
+  for (const {symbol, value} of values){
+    while (year >= value){ 
+      result += symbol; //acrescentando símbolo correspondente à string result;
+      year -= value; //diminuindo valores do ano conforme array;
+    }
+  }
+
+  return result
+}
+
+console.log(convertToRoman(1990));
